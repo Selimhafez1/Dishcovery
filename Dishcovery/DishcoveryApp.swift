56172e7fccc230a4d7d9057608a19ccf5cf25c2c
Selimhafez1 <selimhafez7@gmail.com>
2025-10-23 17:34:29 +0300

    Initial Commit

---
+++ Dishcovery/DishcoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishcoveryApp.swift
+//  Dishcovery
+//
+//  Created by Selim Hafez on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DishcoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
